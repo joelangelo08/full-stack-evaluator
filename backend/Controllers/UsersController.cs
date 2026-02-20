8fd1bd8bad84395670ed239b76fff71990fd5eaa
Joel Angelo Milañez <93751926+joelangelo08@users.noreply.github.com>
2026-02-20 21:03:13 +0800

    Added UsersController for fetching

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+using TaskManager.Models;
+using TaskManager.Data;
+namespace TaskManager.API
+{
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            
+            var users = await _context.Users.OrderBy(t => t.Id).ToListAsync();
+            return Ok(users);
+        }
+    }
+}
