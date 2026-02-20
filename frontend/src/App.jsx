7ea47540b5e7ead16eed5385d79c0e1bf7b97e0c
Joel Angelo Milañez <93751926+joelangelo08@users.noreply.github.com>
2026-02-20 21:10:00 +0800

    Frontend minor updates (css and page)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tasks from "./Tasks"
+import CreateTask from "./CreateTask";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="app">
       <h1>📝 React Task Evaluator</h1>
-      <Tasks />
+    
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" />} />
+
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/tasks/create" element={<CreateTask />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
